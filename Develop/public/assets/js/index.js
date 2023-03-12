56c94fdb5dc7b703a0a96dbe1562c112348b4442
Greg Griffith <griffith_g@hotmail.com>
2023-03-12 00:50:21 -0500

    adding comments on server.js

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -5,14 +5,12 @@ let newNoteBtn;
 let noteList;
 
 if (window.location.pathname === '/notes') {
-  console.log("this line");
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
   newNoteBtn = document.querySelector('.new-note');
   noteList = document.querySelectorAll('.list-container .list-group');
 }
-//console.log(window.location.pathname);
 
 // Show an element
 const show = (elem) => {
@@ -121,7 +119,7 @@ const handleRenderSaveBtn = () => {
 // Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
-  if (window.location.pathname === '../../notes.html') {
+  if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
 
@@ -167,7 +165,7 @@ const renderNoteList = async (notes) => {
     noteListItems.push(li);
   });
 
-  if (window.location.pathname === '../../notes.html') {
+  if (window.location.pathname === '/notes') {
     noteListItems.forEach((note) => noteList[0].append(note));
   }
 };
@@ -175,7 +173,7 @@ const renderNoteList = async (notes) => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
-if (window.location.pathname === "'../../notes.html'") {
+if (window.location.pathname === '/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
--- Develop/server.js
+++ Develop/server.js
@@ -1,23 +1,48 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-//const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT||3001;
 const app = express();
 
+let notes = require('/db/db.json');
+
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//midleware for serving assets in the public folder
 app.use(express.static('public'));
 
+//GET route for home page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+//GET route for notes
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')));
 
-app.post('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')));
+//GEt route for retreving all notes
+app.get('/api/notes', (req, res) => res.json(notes));
 
-//app.delete('/api/notes/:id')
+//POSt route for submitting new notes
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = req.body;
+    notes.push(note);
+    fs.writeFileSync(path.join(__dirname, '/db/db.json'),JSON.stringify(notes,null,2));
+    res.json(note);
+});
+
+//DELETE route for notes
+app.delete('/api/notes/:id', (req, res) => {
+    notes = notes.filter(note => note.id !== req.params.id);
+    fs.writeFileSync(path.join(__dirname, '/db/db.json'),JSON.stringify(notes,null,2));
+    res.json(notes);
+});
+
+//wildcard route for invalid enpoints
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
